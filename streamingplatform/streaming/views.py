d6a3091f7adbc060bb09468ed7a6407fe892dab6
hmeljuice <strlekar.blaz@gmail.com>
2022-02-03 16:32:34 +0100

    next time need to fix detail view video

--- streamingplatform/streaming/views.py
+++ streamingplatform/streaming/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Video
+from django.views import generic
 
 # Create your views here.
 def index(request):
     """View function for home page"""
+    videos = Video.objects.all()
 
-    return render(request, 'index.html')
+    context = {
+        'videos': videos,
+    }
+
+    return render(request, 'index.html', context)
+
+
+class VideoDetailView(generic.DetailView):
+    model = Video
